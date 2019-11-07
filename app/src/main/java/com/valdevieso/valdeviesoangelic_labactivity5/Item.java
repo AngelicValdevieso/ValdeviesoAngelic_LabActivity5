0b3e2d6c0e2e0d6e74b48589baba0aac089b28d4
AngelicValdevieso <2015081658@ust-ics.mygbiz.com>
2019-11-07 23:20:33 +0800

    Initial commit

---
+++ app/src/main/java/com/valdevieso/valdeviesoangelic_labactivity5/Item.java
@@ -0,0 +1,14 @@
+package com.valdevieso.valdeviesoangelic_labactivity5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item);
+    }
+}
